e46567fe491c9909628c7c6fcd3b4bb94889fcfb
XB Pham <xbphamvn@gmail.com>
2020-12-30 10:00:35 +0700

    A reactjs project uses react-redux!

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BaiTapBurgerRedux from './BurgerOrder/BurgerOrder';
+
+function App() {
+  return (
+    <div className="App">
+      <BaiTapBurgerRedux />
+    </div>
+  );
+}
+
+export default App;
